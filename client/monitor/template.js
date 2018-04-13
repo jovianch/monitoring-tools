d0b46de7821780a2ac7fdd45f0f5fa823414e170
Jovian Christianto <christiantojovian@gmail.com>
2018-04-13 13:59:10 +0700

    show checklists, add event click

--- client/monitor/template.js
+++ client/monitor/template.js
@@ -12,7 +12,7 @@ Meteor.subscribe('Projects');
 // };
 
 Template.monitor.helpers({
-	myobject : function() {
+	alphas : function() {
 		return Projects.findOne().alphas
 	}
 });
@@ -20,6 +20,41 @@ Template.monitor.helpers({
 Template.registerHelper('arrayify',function(obj){
     var result = [];
     for (var key in obj) result.push({name:key,value:obj[key]});
-    console.log(result);
     return result;
 });
+
+Template.monitor.events({
+	'click': function(){
+        console.log("You clicked something");
+    },
+    'click .alphas': function(event) {
+    	Session.set('states', event.currentTarget.id);
+    	var states = Session.get('states');
+        $("." + states).css('visibility', 'hidden');
+    },
+    'click .states': function(event) {
+    	Session.set('states', event.currentTarget.id);
+    	var states = Session.get('states');
+    	var fields = {}
+    	var appendString = "alphas.Opportunity.states." + states + ".result";
+    	fields[appendString] = "true";
+    	console.log(appendString);
+    	Projects.update({_id:"CyctMixX7iiThhNvW"}, {$set : fields});
+    }
+
+});
+
+function asArray(obj){
+    var result = [];
+    for (var key in obj){
+        if (obj[key].hasOwnProperty('order'))
+            result.push({name:key,value:obj[key],order:obj[key].order});
+        else
+            result.push({name:key,value:obj[key]});
+    }
+
+    if (result != null && result[0].hasOwnProperty('order'))
+            return result.sort(function(a,b) { return parseFloat(a.order) - parseFloat(b.order) } );
+        else
+            return result;
+    }
