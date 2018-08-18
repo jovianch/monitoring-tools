c4780d4a5ca994bdbc97902b9c73146a3b538d8e
Jovian Christianto <christiantojovian@gmail.com>
2018-08-18 14:08:42 +0700

    subalpha for state, and end date in activity

--- client/update/update.js
+++ client/update/update.js
@@ -14,6 +14,14 @@ Template.update.helpers({
     activities : function() {
         var id = Session.get('project');
         return Projects.findOne({_id:id}).activityspaces
+    },
+    is_subalpha : function(alpha) {
+        var arr_subalpha = arrayify(alpha.subalphas);
+        if (arr_subalpha.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 });
 
@@ -48,89 +56,60 @@ Template.update.events({
     	Session.set('states', event.currentTarget.id);
     	var states = Session.get('states');
         var string_alphas = 'alpha' + states;
+        console.log(string_alphas);
         var alphas = document.getElementById(string_alphas).value;
         alphas = alphas.replace(/\s/g,'');
+        console.log('alphas : ' + alphas);
         var id = Session.get('project')
         var project = Projects.findOne({_id:id});
-        var arr_states = arrayify(project.alphas[alphas].states);
+
+        var arr_subalpha = arrayify(project.alphas[alphas].subalphas);
+        if (arr_subalpha.length > 0) {
+            var string_subalpha = 'subalpha' + states;
+            var subalpha = document.getElementById(string_subalpha).value;
+            subalpha = subalpha.replace(/\s/g,'');
+            var arr_states = arrayify(project.alphas[alphas].subalphas[subalpha].states);
         
-        var idx_states = 0;
-        var i = 0;
-        var is_states = false;
-        while (!(is_states)) {
-            if (arr_states[i].name === project.alphas[alphas].states[states].name.replace(/\s/g,'')) {
-                idx_states = i + 1;
-                is_states = true;
-            }
-            i++;
-        }
-
-        var arr_activity_spaces = arrayify(project.activityspaces);
-
-        console.log('idx_states : ' + idx_states);
-        if (project.alphas[alphas].states[states].result) {
-            for (var j = arr_states.length; j >= idx_states; j--) {
-                var fields = {}
-                var appendString = "alphas." + alphas + ".states." + arr_states[j-1].name + ".result";
-                fields[appendString] = false;
-                var appendString = "alphas." + alphas + ".states." + arr_states[j-1].name + ".timestamp";
-                fields[appendString] = new Date();
-                console.log(fields)
-                Projects.update({_id:id}, {$set : fields});
-            }
-        } else {
-            for (var j = 0; j < idx_states; j++) {
-                var fields = {}
-                var appendString = "alphas." + alphas + ".states." + arr_states[j].name + ".result";
-                fields[appendString] = true;
-                var appendString = "alphas." + alphas + ".states." + arr_states[j].name + ".timestamp";
-                fields[appendString] = new Date();
-                console.log(fields);
-                Projects.update({_id:id}, {$set : fields});
-                // console.log(project)
-            }
-        }
-
-        arr_activity_spaces.forEach(function(activity_space) {
-            var completion_criterias = activity_space.value.completioncriteria;
-            var is_complete = true;
-            var id = Session.get('project');
-            var project = Projects.findOne({_id:id});
-            // console.log("activity_space : " + activity_space.name);
-            completion_criterias.forEach(function(completion_criteria) {
-                var alphas = completion_criteria.split('::')[0].replace(/ +/g, "");; //o sbelum, 1 sesudah
-                var states = completion_criteria.split('::')[1].replace(/ +/g, "");;
-                // console.log("alphas : " + alphas);
-                // console.log("states : " + states);
-                // console.log(project.alphas[alphas].states[states].result);
-                if (project.alphas[alphas].states[states].result) {
-                    is_complete = is_complete && true;
-                } else {
-                    is_complete = is_complete && false;
+            var idx_states = 0;
+            var i = 0;
+            var is_states = false;
+            while (!(is_states)) {
+                if (arr_states[i].name.replace(/\s/g,'') === project.alphas[alphas].subalphas[subalpha].states[states].name.replace(/\s/g,'')) {
+                    idx_states = i + 1;
+                    is_states = true;
                 }
-            });
-            // console.log(is_complete);
-            if (is_complete) {
-                var fields = {}
-                var appendString = "activityspaces." + activity_space.name + ".result";
-                fields[appendString] = true;
-                var appendString = "activityspaces." + activity_space.name + ".timestamp";
-                fields[appendString] = new Date();
-                var id = Session.get('project');
-                Projects.update({_id:id}, {$set : fields});
-            } else {
-                var fields = {}
-                var appendString = "activityspaces." + activity_space.name + ".result";
-                fields[appendString] = false;
-                var appendString = "activityspaces." + activity_space.name + ".timestamp";
-                fields[appendString] = new Date();
-                var id = Session.get('project');
-                Projects.update({_id:id}, {$set : fields});
+                i++;
             }
 
-            var arr_activities = arrayify(activity_space.value.activities);
-            arr_activities.forEach(function(activity) {
-                var completion_criterias = activity.value.completioncriteria;
+            var arr_activity_spaces = arrayify(project.activityspaces);
+
+            console.log('idx_states : ' + idx_states);
+            if (project.alphas[alphas].subalphas[subalpha].states[states].result) {
+                for (var j = arr_states.length; j >= idx_states; j--) {
+                    var fields = {}
+                    var appendString = "alphas." + alphas + ".subalphas." + subalpha + ".states." + arr_states[j-1].name + ".result";
+                    fields[appendString] = false;
+                    var appendString = "alphas." + alphas + ".subalphas." + subalpha + ".states." + arr_states[j-1].name + ".timestamp";
+                    fields[appendString] = new Date();
+                    console.log(fields)
+                    Projects.update({_id:id}, {$set : fields});
+                }
+            } else {
+                for (var j = 0; j < idx_states; j++) {
+                    var fields = {}
+                    var appendString = "alphas." + alphas + ".subalphas." + subalpha + ".states." + arr_states[j].name + ".result";
+                    fields[appendString] = true;
+                    var appendString = "alphas." + alphas + ".subalphas." + subalpha + ".states." + arr_states[j].name + ".timestamp";
+                    fields[appendString] = new Date();
+                    console.log(fields);
+                    Projects.update({_id:id}, {$set : fields});
+                    // console.log(project)
+                }
+            }
+
+            //INI BELUM
+            arr_activity_spaces.forEach(function(activity_space) {
+                var completion_criterias = activity_space.value.completioncriteria;
                 var is_complete = true;
                 var id = Session.get('project');
                 var project = Projects.findOne({_id:id});
@@ -150,23 +129,179 @@ Template.update.events({
                 // console.log(is_complete);
                 if (is_complete) {
                     var fields = {}
-                    var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                    var appendString = "activityspaces." + activity_space.name + ".result";
                     fields[appendString] = true;
-                    var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                    var appendString = "activityspaces." + activity_space.name + ".timestamp";
                     fields[appendString] = new Date();
                     var id = Session.get('project');
                     Projects.update({_id:id}, {$set : fields});
                 } else {
                     var fields = {}
-                    var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                    var appendString = "activityspaces." + activity_space.name + ".result";
                     fields[appendString] = false;
-                    var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                    var appendString = "activityspaces." + activity_space.name + ".timestamp";
                     fields[appendString] = new Date();
                     var id = Session.get('project');
                     Projects.update({_id:id}, {$set : fields});
                 }
+
+                var arr_activities = arrayify(activity_space.value.activities);
+                arr_activities.forEach(function(activity) {
+                    var completion_criterias = activity.value.completioncriteria;
+                    var is_complete = true;
+                    var id = Session.get('project');
+                    var project = Projects.findOne({_id:id});
+                    // console.log("activity_space : " + activity_space.name);
+                    completion_criterias.forEach(function(completion_criteria) {
+                        var alphas = completion_criteria.split('::')[0].replace(/ +/g, "");; //o sbelum, 1 sesudah
+                        var states = completion_criteria.split('::')[1].replace(/ +/g, "");;
+                        // console.log("alphas : " + alphas);
+                        // console.log("states : " + states);
+                        // console.log(project.alphas[alphas].states[states].result);
+                        if (project.alphas[alphas].states[states].result) {
+                            is_complete = is_complete && true;
+                        } else {
+                            is_complete = is_complete && false;
+                        }
+                    });
+                    // console.log(is_complete);
+                    if (is_complete) {
+                        var fields = {}
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                        fields[appendString] = true;
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                        fields[appendString] = new Date();
+                        var id = Session.get('project');
+                        Projects.update({_id:id}, {$set : fields});
+                    } else {
+                        var fields = {}
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                        fields[appendString] = false;
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                        fields[appendString] = new Date();
+                        var id = Session.get('project');
+                        Projects.update({_id:id}, {$set : fields});
+                    }
+                });
             });
-        })
+        } else {
+            var arr_states = arrayify(project.alphas[alphas].states);
+        
+            var idx_states = 0;
+            var i = 0;
+            var is_states = false;
+            while (!(is_states)) {
+                if (arr_states[i].name === project.alphas[alphas].states[states].name.replace(/\s/g,'')) {
+                    idx_states = i + 1;
+                    is_states = true;
+                }
+                i++;
+            }
+
+            var arr_activity_spaces = arrayify(project.activityspaces);
+
+            console.log('idx_states : ' + idx_states);
+            if (project.alphas[alphas].states[states].result) {
+                for (var j = arr_states.length; j >= idx_states; j--) {
+                    var fields = {}
+                    var appendString = "alphas." + alphas + ".states." + arr_states[j-1].name + ".result";
+                    fields[appendString] = false;
+                    var appendString = "alphas." + alphas + ".states." + arr_states[j-1].name + ".timestamp";
+                    fields[appendString] = new Date();
+                    console.log(fields)
+                    Projects.update({_id:id}, {$set : fields});
+                }
+            } else {
+                for (var j = 0; j < idx_states; j++) {
+                    var fields = {}
+                    var appendString = "alphas." + alphas + ".states." + arr_states[j].name + ".result";
+                    fields[appendString] = true;
+                    var appendString = "alphas." + alphas + ".states." + arr_states[j].name + ".timestamp";
+                    fields[appendString] = new Date();
+                    console.log(fields);
+                    Projects.update({_id:id}, {$set : fields});
+                    // console.log(project)
+                }
+            }
+
+            arr_activity_spaces.forEach(function(activity_space) {
+                var completion_criterias = activity_space.value.completioncriteria;
+                var is_complete = true;
+                var id = Session.get('project');
+                var project = Projects.findOne({_id:id});
+                // console.log("activity_space : " + activity_space.name);
+                completion_criterias.forEach(function(completion_criteria) {
+                    var alphas = completion_criteria.split('::')[0].replace(/ +/g, "");; //o sbelum, 1 sesudah
+                    var states = completion_criteria.split('::')[1].replace(/ +/g, "");;
+                    // console.log("alphas : " + alphas);
+                    // console.log("states : " + states);
+                    // console.log(project.alphas[alphas].states[states].result);
+                    if (project.alphas[alphas].states[states].result) {
+                        is_complete = is_complete && true;
+                    } else {
+                        is_complete = is_complete && false;
+                    }
+                });
+                // console.log(is_complete);
+                if (is_complete) {
+                    var fields = {}
+                    var appendString = "activityspaces." + activity_space.name + ".result";
+                    fields[appendString] = true;
+                    var appendString = "activityspaces." + activity_space.name + ".timestamp";
+                    fields[appendString] = new Date();
+                    var id = Session.get('project');
+                    Projects.update({_id:id}, {$set : fields});
+                } else {
+                    var fields = {}
+                    var appendString = "activityspaces." + activity_space.name + ".result";
+                    fields[appendString] = false;
+                    var appendString = "activityspaces." + activity_space.name + ".timestamp";
+                    fields[appendString] = new Date();
+                    var id = Session.get('project');
+                    Projects.update({_id:id}, {$set : fields});
+                }
+
+                var arr_activities = arrayify(activity_space.value.activities);
+                arr_activities.forEach(function(activity) {
+                    var completion_criterias = activity.value.completioncriteria;
+                    var is_complete = true;
+                    var id = Session.get('project');
+                    var project = Projects.findOne({_id:id});
+                    // console.log("activity_space : " + activity_space.name);
+                    completion_criterias.forEach(function(completion_criteria) {
+                        var alphas = completion_criteria.split('::')[0].replace(/ +/g, "");; //o sbelum, 1 sesudah
+                        var states = completion_criteria.split('::')[1].replace(/ +/g, "");;
+                        // console.log("alphas : " + alphas);
+                        // console.log("states : " + states);
+                        // console.log(project.alphas[alphas].states[states].result);
+                        if (project.alphas[alphas].states[states].result) {
+                            is_complete = is_complete && true;
+                        } else {
+                            is_complete = is_complete && false;
+                        }
+                    });
+                    // console.log(is_complete);
+                    if (is_complete) {
+                        var fields = {}
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                        fields[appendString] = true;
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                        fields[appendString] = new Date();
+                        var id = Session.get('project');
+                        Projects.update({_id:id}, {$set : fields});
+                    } else {
+                        var fields = {}
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".result";
+                        fields[appendString] = false;
+                        var appendString = "activityspaces." + activity_space.name + ".activities." + activity.name + ".timestamp";
+                        fields[appendString] = new Date();
+                        var id = Session.get('project');
+                        Projects.update({_id:id}, {$set : fields});
+                    }
+                });
+            });
+        }
+        
     },
     'click .activity_spaces': function(event) {
         Session.set('activity', event.currentTarget.id);
@@ -397,7 +532,16 @@ Template.update.events({
         var string_alphas = 'alpha' + states;
         var alphas = document.getElementById(string_alphas).value;
         alphas = alphas.replace(/\s/g,'');
-        var checklist = project.alphas[alphas].states[states].checklists;
+
+        var arr_subalpha = arrayify(project.alphas[alphas].subalphas);
+        if (arr_subalpha.length > 0) {
+            var string_subalpha = 'subalpha' + states;
+            var subalpha = document.getElementById(string_subalpha).value;
+            subalpha = subalpha.replace(/\s/g,'');
+            var checklist = project.alphas[alphas].subalphas[subalpha].states[states].checklists;
+        } else {
+            var checklist = project.alphas[alphas].states[states].checklists;
+        }
 
         var tbody = document.getElementById('checklist');
 
