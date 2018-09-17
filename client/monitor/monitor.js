be8e86c0b26a30f0ab5935e7cd700895a283af75
Jovian Christianto <christiantojovian@gmail.com>
2018-09-17 13:36:23 +0700

    show all graph

--- client/monitor/monitor.js
+++ client/monitor/monitor.js
@@ -11,6 +11,14 @@ Template.monitor.helpers({
     }
 });
 
+Template.monitor.onRendered(function() {
+    drawAlpha();
+    drawConcern();
+    // drawActivity();
+    drawSubalpha();
+    drawSpider();
+});
+
 Template.registerHelper('arrayify',function(obj){
     var result = [];
     for (var key in obj) result.push({name:key,value:obj[key]});
